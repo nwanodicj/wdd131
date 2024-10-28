50dc3fcd6c900b3b4f8588b2160a63fccbc3bff7
nwanodicj <justicenwanodi2@gmail.com>
2024-10-28 19:32:55 +0100

    update

--- scripts/form.js
+++ scripts/form.js
@@ -1,30 +1,30 @@
 const product = [
 
-    {
-      id: "fc-1888",
-      name: "flux capacitor",
-      averagerating: 4.5
-    },
-    {
-      id: "fc-2050",
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: "fs-1987",
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: "ac-2000",
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: "jj-1969",
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
 //..........Inner HTML Here.........
@@ -33,30 +33,53 @@ let productHTML = "";
 product.forEach((item) => {
 
 //..........Generate the HTML..........
-    productHTML  += `
-    <select id="productName" name="Product Name" required>
-        <option id=${item.id} name=${item.name} disabled selected></option>
-        <option value=${item.id}>${item.name}</option>
-        
-    </select>`
+  productHTML  += `
+  <div>
+    <option id=${item.id} name=${item.name} disabled selected></option>
+    <option value=${item.id}>${item.name}</option>
+  <div>
+  `
 });
 
 const outPut = document.querySelector('#productName').innerHTML = productHTML
- 
-console.log(outPut)
 
 
 //.........Placeholder Here...........
 const inputF = document.querySelector('#productName');
 
 inputF.addEventListener('focus', function() {
-    inputF.placeholder = "Focused - Select a Product...";
+  inputF.placeholder = "Focused - Select a Product...";
 });
 
 inputF.addEventListener('blur', function() {
-    inputF.placeholder = "Default Placeholder";
+  inputF.placeholder = "Default Placeholder";
 });
 
+//..........Hamburger Here..............
+
+function myFunction() {
+const x = document.querySelector(".nav-bar-container");
+if (x.style.display === "block") {
+  x.style.display = "none";
+} else {
+  x.style.display = "block";
+}
+}
+
+//..............Local Storage........... 
+clickCounter();
+
+function clickCounter() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  
+  let storageEll = document.querySelector(".js-cart-quantity").innerHTML = localStorage.clickcount;
+
+  console.log(storageEll)
+}
 
 // page datetime last modification
 let lastModified = document.querySelector('#last-modification');
@@ -69,22 +92,63 @@ let oLastModif = new Date(document.lastModified);
 let nLastModif = Date.parse(document.lastModified);
 
 lastModified.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		nLastModif: "full",
-	}
+"en-US",
+{
+  nLastModif: "full",
+}
 ).format(nLastModif)
 }`;
 
 lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
 
+//..............Cart Here................
 
-
-  function myFunction() {
-	const x = document.querySelector(".nav-bar-container");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
-	}
-  }
+//const cart = [{
+//
+//}]
+//cart.push(product);
+//
+//function addToCart(name) {
+//  //   the code below is to increase the quantity of one product   
+//  let matchingItem = '';
+//
+//  cart.forEach((cartItem) => {
+//      if (name === cartItem.name) {
+//          matchingItem = cartItem;
+//      }
+//
+//      console.log(matchingItem)
+//  });
+//
+//  if (matchingItem) {
+//    matchingItem.quantity += 1;
+//}else{
+//    cart.push({
+//        name: name,
+//        quantity: 1,
+//    });
+//}
+//}
+//
+//
+//function updateproductReview() {
+//  //The code below shows the total number of products in the cart
+//  let productReview = 0;
+//  product.forEach((cartItem) => {
+//    productReview += cartItem.quantity
+//  });
+//  const productsReview = document.querySelector('.js-cart-quantity')
+//productsReview.innerHTML = productReview
+//};
+//
+//// Below here we make the button interactive by using EventListener
+//document.querySelectorAll('.js-add-to-cart')
+//  .forEach((button) => {
+//      button.addEventListener('click', () => {
+//        "hello world"
+//          const name = button.dataset.productId;
+//          addToCart(name)
+//          updateproductReview()
+//          
+//      });
+//  });
